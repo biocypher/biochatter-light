758d2bef38ac27de6db6fcd4f130ee09f863a94c
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2023-04-17 09:21:10 +0200

    first biomed query

--- query.py
+++ query.py
@@ -1,5 +1,4 @@
 import openai
-import json
 
 # read key from key.txt
 key = open('key.txt', 'r').read()
@@ -10,12 +9,14 @@ MODEL = "gpt-3.5-turbo"
 response = openai.ChatCompletion.create(
     model=MODEL,
     messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Knock knock."},
-        {"role": "assistant", "content": "Who's there?"},
-        {"role": "user", "content": "Orange."},
+        {"role": "system", "content": "You are an assistant to a biomedical researcher."},
+        {"role": "system", "content": "Your role is to contextualise the user's findings with biomedical background knowledge."},
+        {"role": "system", "content": "The topic of the research is high grade serous ovarian cancer."},
+        {"role": "system", "content": "You can ask the user to provide explanations and more background, for instance on the treatment the patient has received."},
+        {"role": "system", "content": "The user has generated experimental results for a patient based on gene expression in the cancerous tissue."},
+        {"role": "user", "content": "In the patient, I found PI3K and AKT to be highly active, while homologous recombination seems to be defective. What could this mean?"},
     ],
     temperature=0,
 )
 
-response
+print(response)
