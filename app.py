2f09e2422888762f7086308e385ae623a79bec51
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2023-04-20 18:45:50 +0200

    page icon (where is that shown, anyway?)

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import streamlit.components.v1 as components
 
 st.set_page_config(
     page_title="ChatGSE",
-    page_icon="🤖",
+    page_icon="💬",
     layout="wide",
     initial_sidebar_state="expanded",
 )
