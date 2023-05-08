57aecf1e834bbf6164bb9ab647a19a50bbc4cb39
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2023-05-08 22:04:06 +0200

    add custom css sheet, fix button sizes

--- app.py
+++ app.py
@@ -16,6 +16,15 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
+
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style.css")
+
 ss = st.session_state
 
 OPENAI_MODELS = [
