dbf038151219a52a07e9371ce7a80e27d2f8d61d
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2023-06-01 01:55:56 +0200

    allow testing of correcting agent functionality .. by text entry

--- app.py
+++ app.py
@@ -996,6 +996,28 @@ def correcting_agent_panel():
         value=False,
     )
 
+    test_correct = st.text_area(
+        "Test correction functionality here",
+        placeholder=(
+            "Enter a false statement to prompt a correction. Press [Enter] for "
+            "a new line, and [CTRL+Enter] or [⌘+Enter] to submit."
+        ),
+    )
+
+    if test_correct:
+        if not ss.get("conversation"):
+            st.write("No model loaded. Please load a model first.")
+            return
+
+        correction = ss.conversation._correct_response(test_correct)
+
+        if str(correction).lower() in ["ok", "ok."]:
+            st.success("The model found no correction to be required.")
+        else:
+            st.error(
+                f"The model found the following correction: `{correction}`"
+            )
+
 
 def main():
     # NEW SESSION
