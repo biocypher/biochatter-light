82ed2537aa0533e4313304ae135fa36ef2458847
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2023-06-14 16:29:18 +0200

    about text update, detailing models

--- app.py
+++ app.py
@@ -563,6 +563,10 @@ def app_info():
     st.markdown(
         """
         
+        ChatGSE is developed by [Sebastian
+        Lobentanzer](https://slobentanzer.github.io); you can find the source
+        code on [GitHub](https://github.com/biocypher/chatgse).
+
         ChatGSE is a tool to rapidly contextualise common end results of
         biomedical analyses. It works by setting up a topic-constrained
         conversation with a pre-trained language model. The main benefits of
@@ -596,9 +600,17 @@ def app_info():
         and thus can be useful to explain the biological context of, for
         instance, a particular gene set or cell type.
 
-        ChatGSE is developed by [Sebastian
-        Lobentanzer](https://slobentanzer.github.io); you can find the source
-        code on [GitHub](https://github.com/biocypher/chatgse).
+        ## About the models
+        The default model loaded is OpenAIs `gpt-3.5-turbo` model, which in the
+        standard version has a token limit of 4000 per conversation. This model
+        currently comes in two versions, `0301` and `0613`. The latter is the
+        more recent one with improved interpretation of system messages and
+        capabilities to handle functions (returning attribute values of a given
+        function as JSON). Additionally, OpenAI provide a `gpt-3.5-turbo-16k`
+        model with increased token limit of 16000 per conversation. This model
+        is slightly more expensive, but can be useful for longer conversations,
+        particularly when including the document summarisation / prompt
+        injection feature.
 
         """
     )
