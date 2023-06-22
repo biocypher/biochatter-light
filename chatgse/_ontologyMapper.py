9df697e6dddc49e9a46d69a9acf3fdcfab82c715
Roman Mähler <roman.maehler@hs-heilbronn.de>
2023-06-22 18:20:56 +0200

    Added button functionallity

--- chatgse/_ontologyMapper.py
+++ chatgse/_ontologyMapper.py
@@ -20,6 +20,14 @@ class OntologyMapper:
     def __init__(self):
         if "terms" not in ss:
             ss.terms = ""
+        if "coulumn_Names" not in ss:
+            ss.coulumn_Names = ['celltypes', 'cell', 'cells', 'Zelltypen', 'Zelle', 'Zellen']
+        if "map" not in ss:
+            ss.map = False
+        if "term_list" not in ss:
+            ss.term_list = []
+        if "mapped_term_list" not in ss:
+            ss.mapped_term_list = []
 
     @staticmethod
     def _render_msg(role: str, msg: str):
@@ -47,25 +55,74 @@ class OntologyMapper:
         #extracting the terms and put them into a list
         #use ; , and \n as split between words
         #TODO: possibility to get the words from a file
+        
+        #List of words that will be mapped
+        celltypes = []
+
+        if ss.get("ontology_tool_data"):
+
+            #reading the csv files
+            dataframes = []
+            for f in ss.ontology_tool_data:
+                df_csv = pd.read_csv(f)
+                dataframes.append(df_csv)
+
+    	    #Extract the possible filenames and drop the NAN values and the punctuation:
+
+            for df in dataframes:
+                for name in ss.coulumn_Names:
+                    #check if coulumn name exists
+                    if name in df.columns:
+                    
+                        #drop nans
+                        df = df.dropna(subset=name)
+
+                        #getting celltypes, removing the punctuation and convert it to a list
+                        celltypes = celltypes + df[name].str.replace('[{}]'.format(string.punctuation), '').tolist()
+
+        
+        #Load Terms from ChatBox
         terms = ss.terms.replace(';', '\n').replace(',', '\n').replace('[{}]'.format(string.punctuation), '').split('\n')
 
+        #Adding terms from the Box to the celltypes of the file
+        celltypes = celltypes + terms
+        
         #caching the ontologie if not exists
         if(not text2term.cache_exists('CL')):
             self.cache_ontologies()
 
         #map the terms
         #TODO: Optional possibility to save the mappings
-        result = text2term.map_terms(terms, "CL", base_iris="http://purl.obolibrary.org/obo/CL",  use_cache=True)
+        result = text2term.map_terms(celltypes, "CL", base_iris="http://purl.obolibrary.org/obo/CL",  use_cache=True)
 
-        self._write_and_history("💬🧬 text2term", "mapping results",)
-        st.markdown(
-        f"""
-        ```
-        {result.to_markdown()}
-        """
+        #self._write_and_history("💬🧬 text2term", "mapping results",)
+        #st.markdown(
+        #f"""
+        #```
+        #{result.to_markdown()}
+        #"""
         
-        )
+        #)
         #For design
-        st.markdown("\n")
+        #st.markdown("\n")
 
-        return result
+        #return result
+
+        build_term_lists(result)
+    
+def build_term_lists(result):
+    #maybe it would be better to use dictionary 
+    ss.term_list = []
+    ss.mapped_term_list = []
+    for index, row in result.iterrows():
+        term = result['Source Term'][index]
+        if(term in ss.term_list):
+            term_index = ss.term_list.index(term)
+            ss.mapped_term_list[term_index].append((row['Mapped Term Label'], row['Mapping Score'])) 
+        else:
+            ss.term_list.append(term)
+            mapped_Term = []
+            mapped_Term.append((row['Mapped Term Label'], row['Mapping Score']))
+            ss.mapped_term_list.append(mapped_Term)
+
+
