88f1956e4264bd5736df4210e1aa3b3b771cb90e
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2023-09-28 16:05:37 +0200

    formatting

--- app.py
+++ app.py
@@ -1324,25 +1324,44 @@ def genetics_panel():
 
 
 def gene_panel():
-    gene_name = st.text_input("Enter gene name:")
+    gene_name = st.text_input("Enter gene name (case-insensitive):")
 
     # if input, show table
     if gene_name:
         # get data
-        cn_df, vn_df = _get_gene_data(gene_name)
+        cn_df, vn_df = _get_gene_data(gene_name.upper())
+
+        cnas = 0
+        vnas = 0
+
+        cna_text = "CNAs"
+        if cn_df is not None:
+            cnas = cn_df.shape[0]
+            if cnas == 1:
+                cna_text = "CNA"
+
+        vna_text = "variants"
+        if vn_df is not None:
+            vnas = vn_df.shape[0]
+            if vnas == 1:
+                vna_text = "variant"
 
         # genomespy link
         st.markdown(
+            f"{cnas} {cna_text}, {vnas} {vna_text}. Links: "
             f"[GenomeSpy](https://genomespy.app/decider/gene/{gene_name}) "
-            f"({gene_name}) "
-            f"({cn_df.shape[0]} CNAs, {vn_df.shape[0]} variants)"
         )
 
-        # show tables (no row numbers)
-        st.markdown("#### Copy Number Alterations")
-        st.dataframe(cn_df, hide_index=False)
-        st.markdown("#### Sequence Variants")
-        st.dataframe(vn_df, hide_index=False)
+        if cn_df is None and vn_df is None:
+            st.write("No data found for this gene.")
+
+        if cn_df is not None:
+            st.markdown("#### Copy Number Alterations")
+            st.dataframe(cn_df, hide_index=False)
+
+        if vn_df is not None:
+            st.markdown("#### Sequence Variants")
+            st.dataframe(vn_df, hide_index=False)
 
 
 def _get_gene_data(gene_name):
@@ -1368,6 +1387,9 @@ def _get_gene_data(gene_name):
         "ss.id ",
         gene_id=gene_id,
     )
+
+    if not result[0]:
+        return None, None
 
     patterns = result[0]
     # header: gene information (should be the same gene in all rows)
